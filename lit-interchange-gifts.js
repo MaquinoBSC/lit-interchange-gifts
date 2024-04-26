881edfab8a69de9cff0de84fed70ebfe60519de5
MaquinoBSC <martinare.ipn@gmail.com>
2024-04-26 15:00:41 -0600

    chore: creating of the component

---
+++ lit-interchange-gifts.js
@@ -0,0 +1,11 @@
+import { LitElement, html } from "lit";
+
+class LitInterchangeGifts extends LitElement {
+    render() {
+        return html`
+            <p>Martin</p>
+        `;
+    }
+}
+
+customElements.define('lit-interchange-gifts', LitInterchangeGifts);
